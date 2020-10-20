1625184e411cb9b91b9c7ef42609a7230b3e5bb8
Sylvain Garcia <sylvain.garcia@ecential.com>
2020-10-20 17:08:40 +0200

    install Findliburing.cmake

--- cmake/cppcoroConfig.cmake
+++ cmake/cppcoroConfig.cmake
@@ -3,4 +3,8 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 include(CMakeFindDependencyMacro)
 find_dependency(CppcoroCoroutines QUIET REQUIRED)
 
+if(@CPPCORO_USE_IO_RING@)
+  find_dependency(liburing QUIET REQUIRED)
+endif()
+
 include("${CMAKE_CURRENT_LIST_DIR}/cppcoroTargets.cmake")
